29127dbbcc5e7880ab85e691036420ce0784c64c
EllisJLC <jia.liang.chen@ryerson.ca>
2024-11-15 22:25:06 -0500

    Added validators

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -1,6 +1,8 @@
 const { checkSchema, validationResult, body} = require('express-validator');
 const User = require('../models/User');
 
+const BadRequestError = require('./customErrors');
+
 const withValidationErrors = (validateValue: {}) => {
   return [
     validateValue,
@@ -8,8 +10,54 @@ const withValidationErrors = (validateValue: {}) => {
       const errors = validationResult(req);
       if(!errors.isEmpty()) {
         const errorMessages = errors.array().map((error) => error.msg);
-        throw new Error
+        throw new BadRequestError(errorMessages)
       }
     }
   ]
 }
+
+const validateRegistration = withValidationErrors([
+  body('firstName').notEmpty().withMessage('First name required'),
+  body('lastName').notEmpty().withMessage('Last name required'),
+  body('email')
+    .notEmpty()
+    .withMessage('Email required')
+    .isEmail()
+    .withMessage('Email is invalid')
+    .custom(async (email: string) => {
+      const user = await User.findOne({ email });
+      if (user) {
+        throw new BadRequestError('Email already in use');
+      }
+    }),
+  body('password')
+    .notEmpty()
+    .withMessage('Please enter an email')
+    .custom( async (password: string) => {
+      if(password.length <= 6) {
+        throw new BadRequestError('Password must be at least 6 characters long')
+      }
+    })
+]);
+
+const validateLogin = withValidationErrors([
+  body('email')
+    .notEmpty()
+    .withMessage('Enter an email')
+    .isEmail()
+    .withMessage('Email invalid'),
+  body('password').notEmpty().withMessage('Please enter your password')
+]);
+
+const validatePost = withValidationErrors([
+  body('content').notEmpty().withMessage('Please enter something to post'),
+  body('postedBy')
+    .notEmpty()
+    .withMessage('Poster unknown')
+    .custom(async (userId: string) => {
+      const user = await User.findOne({ userId });
+      if (!user) {
+        throw new BadRequestError('User unknown');
+      }
+    })
+])
